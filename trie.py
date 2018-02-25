9644103c17041c0b70b3447641d4d30d42e44051
alex <sanyache75@gmail.com>
2018-02-25 15:26:52 +0200

    prefix

--- trie.py
+++ trie.py
@@ -36,11 +36,16 @@ class Trie:
         else:
             rez = 'No'
         return rez
-    def get_trie(self):
-        visitList = []
+    def get_trie(self, node=None, prefix=None):
+
         dict_word = []
-        letter= ''
-        stack = [self.root]
+        if prefix == None:
+            letter= ''
+        else:
+            letter= prefix[:-1]
+        if node == None:
+            node = self.root
+        stack = [node]
         while stack:
             cur = stack.pop()
             if cur == -1:
@@ -53,8 +58,16 @@ class Trie:
                 stack.append(nod)
             if cur.is_word:
                 dict_word.append(letter)
-            visitList.append(cur.data)
         return dict_word
+    def prefix_list(self, prefix):
+        cur = self.root
+        for char in prefix:
+            if char in cur.children:
+                cur = cur.children[char]
+            else:
+                print ('dictionary does not have "%s" word'%(prefix))
+                return
+        return  self.get_trie(cur, prefix)
 trie = Trie()
 words = 'foo foobar foot footer bo boss'
 for word in words.split():
@@ -62,4 +75,5 @@ for word in words.split():
 
 print(trie.his_word('bar'))
 print trie.get_trie()
-
+print trie.prefix_list('fo')
+print trie.prefix_list('fool')
